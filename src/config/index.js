06c406fc6e139bcd1aa1be95d2c6659712ca1ea2
tonnyhawk93 <tonnyhawk93@gmail.com>
2021-10-01 18:39:47 +0300

    add makedir db in config.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,15 @@
 const path = require('path');
+const fs = require('fs');
 const dbFolder = path.resolve(__dirname, '../db/')
 const imgFolder = path.resolve(dbFolder, './images/');
 const dbDumpFile = path.resolve(dbFolder, './dump.json');
+
+try {
+  fs.mkdirSync(dbFolder);
+  fs.mkdirSync(imgFolder);
+}catch {
+  
+}
 
 module.exports = {
     PORT: 8080,
