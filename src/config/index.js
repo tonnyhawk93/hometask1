3e8744a09e4eed0e209193756975dd4dc558146e
tonnyhawk93 <tonnyhawk93@gmail.com>
2021-10-01 23:20:37 +0300

    add db folder

--- src/config/index.js
+++ src/config/index.js
@@ -4,13 +4,6 @@ const dbFolder = path.resolve(__dirname, '../db/')
 const imgFolder = path.resolve(dbFolder, './images/');
 const dbDumpFile = path.resolve(dbFolder, './dump.json');
 
-try {
-  fs.mkdirSync(dbFolder);
-  fs.mkdirSync(imgFolder);
-}catch {
-  
-}
-
 module.exports = {
     PORT: 8080,
     dbFolder,  
